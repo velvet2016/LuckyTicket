0912f25908e2cad3783b58a2c170db9ac138d924
velvet2016 <astudzinsky@gmail.com>
2017-03-02 12:53:47 +0200

    small refactoring

--- src/LuckyTickets.java
+++ src/LuckyTickets.java
@@ -1,11 +1,11 @@
 import java.util.stream.IntStream;
 
-public class Main {
+public class LuckyTickets {
 
     public static void main(String[] args) {
         long count = IntStream
                 .range(0, 1000000)
-                .filter(Main::isLuckyNumber)
+                .filter(i->isLuckyNumber(i))
                 .count();
         System.out.println(count);
         //result: 55252
@@ -13,11 +13,11 @@ public class Main {
 
     private static boolean isLuckyNumber(int inputInt) {
         String formatted = String.format("%06d", inputInt);
-        int left  = getIntByPos(formatted, 0) + getIntByPos(formatted, 1) + getIntByPos(formatted, 2);
-        int right  = getIntByPos(formatted, 3) + getIntByPos(formatted, 4) + getIntByPos(formatted, 5);
+        int left  = getDigitByPos(formatted, 0) + getDigitByPos(formatted, 1) + getDigitByPos(formatted, 2);
+        int right  = getDigitByPos(formatted, 3) + getDigitByPos(formatted, 4) + getDigitByPos(formatted, 5);
         return left == right;
     }
-    private static int getIntByPos(String str, int pos){
+    private static int getDigitByPos(String str, int pos){
         return Character.getNumericValue(str.charAt(pos));
     }
 }
